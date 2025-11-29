6139c880c1e52580784164300afdb3a2961fe289
Akari <s24C1068FQ@s.chibakoudai.jp>
2025-11-30 00:54:12 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,15 +1,41 @@
-#!/bin/bash -xv 
+#/bin/bash -xv 
 #SPDX-FileCopyrightText: 2025 Akari Sunagawa
 #SPDX-License-Indentifier: BSD-3-Clause
-ng () {
-	echo ${1}行目が違うよ
-	res=1
+
+ng() {
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
 
-out=$(seq 5 | python3 ./sysk1.py)
-[ "${out}" = 15 ] || ng "$LINENO"
+INPUT="aabccde"
 
-[ "${res}" = 0 ] && echo OK
+echo "$INPUT" | python3 ./sysk1.py
+
+
+EXPECTED=$(cat <<EOF
+o 4
+e 3
+t 2
+l 2
+n 1
+m 1
+i 1
+h 1
+c 1
+EOF
+)
+
+# 実際の出力を読み込み
+OUTPUT=$(cat freq.txt)
+
+# 比較
+if [ "$OUTPUT" != "$EXPECTED" ]; then
+    ng "$LINENO"
+fi
+
+# 結果
+[ "${res}" = 0 ] && echo "OK"
+
 exit $res
